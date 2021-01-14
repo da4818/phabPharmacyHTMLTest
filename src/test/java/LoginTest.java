bf72dbac1819151d37e24ada654f0a87ea654665
da4818 <diadegoke@gmail.com>
2021-01-14 18:23:51 +0000

    Unit testing the HTML page

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -13,31 +13,65 @@ public class LoginTest {
     public void displayTitle() throws Exception{
         WebDriver driver = new HtmlUnitDriver();
         driver.get("https://phabpharmacy.herokuapp.com/login");
-        Assert.assertEquals(driver.getTitle(),"Login");
+        Assert.assertEquals(driver.getTitle(),"Login"); // Each title matches the URl pattern (except the map, see navigatePages function)
         driver.quit();
     }
     @Test
-    public void displayContent() throws Exception{
+    public void checkLogin() throws Exception{
         WebDriver driver = new HtmlUnitDriver();
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // Set implicit wait
         driver.get("https://phabpharmacy.herokuapp.com/login");
+        // Entering the login details for John Smith
         WebElement email = driver.findElement(By.name("email")); //
         email.sendKeys("js@hotmail.com");
         WebElement password = driver.findElement(By.name("pass"));
         password.sendKeys("qwerty");
         password.submit(); // Submits to the submit button
+        WebElement resp = driver.findElement(By.name("loginResponse"));
+        Assert.assertEquals(resp.getText(),"Welcome back, John!");
         driver.quit();
+
+    }
+    @Test
+    public void checkLogin1() throws Exception{
+        WebDriver driver = new HtmlUnitDriver();
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // Set implicit wait
+        driver.get("https://phabpharmacy.herokuapp.com/login");
+        WebElement email1 = driver.findElement(By.name("email"));
+        email1.sendKeys("mjc72@hotmail.com");
+        WebElement password1 = driver.findElement(By.name("pass"));
+        password1.sendKeys("tiger123");
+        password1.submit();
+        WebElement resp = driver.findElement(By.name("loginResponse"));
+        Assert.assertEquals(resp.getText(),"Welcome back, Mia!");
+        driver.quit();
+        /*email.sendKeys("w-jones12@googlemail.co.uk");
+        password.sendKeys("eXample567");
+        password.submit();
+        Assert.assertEquals(resp.getText(),"Welcome back, Will!");
+        resp = driver.findElement(By.name("loginResponse"));
+        email.sendKeys("cwy@gmail.com");
+        password.sendKeys("password1");
+        password.submit();
+        resp = driver.findElement(By.name("loginResponse"));
+        Assert.assertEquals(resp.getText(),"Welcome back, CW!");
+        email.sendKeys("tpke@gmail.com");
+        password.sendKeys("wasd&&");
+        password.submit();
+        resp = driver.findElement(By.name("loginResponse"));
+        Assert.assertEquals(resp.getText(),"Welcome back, Tim!");*/
     }
     @Test
     public void navigatePages(){
         WebDriver driver = new HtmlUnitDriver();
-        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // Set implicit wait
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // Not needed for this test
         driver.get("https://phabpharmacy.herokuapp.com/login");
         //Back to Homepage
         driver.findElement(By.linkText("Home")).click();
         Assert.assertEquals(driver.getTitle(),"Home");
-        //Back to Browse page
-        driver.findElement(By.name("Browse")).click();
+        //Back to Browse page - having some issues as Browse isn't a hyperlink but a button that calls a function
+        //driver.findElement(By.name("Browse")).click();
+        //Assert.assertEquals(driver.findElement(By.name("Browse")).getText(),"Browse");
         //Assert.assertEquals(driver.getTitle(),"Browse");
         //Back to Register page
         driver.findElement(By.linkText("Register")).click();
@@ -45,8 +79,11 @@ public class LoginTest {
         //Back to Browse page
         driver.findElement(By.linkText("In-Store")).click();
         Assert.assertEquals(driver.getTitle(),"Find Items In-Store");
+        //Back to Browse page
         driver.findElement(By.name("Basket")).click();
         Assert.assertEquals(driver.getTitle(),"Basket");
+
+        // Order, AmendDetails pages aren't accessible through Login - only from Basket
         driver.quit();
     }
 }
