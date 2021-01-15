7137e7f296777e45d25cb79a2f416de9125711c7
mgr1018 <72918389+mgr1018@users.noreply.github.com>
2021-01-15 15:18:56 +0000

    Update MapTest.java

--- src/test/java/MapTest.java
+++ src/test/java/MapTest.java
@@ -1,2 +1,48 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+
+
 public class MapTest {
+    @Test
+    public void displayTitle() throws Exception{
+        WebDriver driver = new HtmlUnitDriver();
+        driver.get("https://phabpharmacy.herokuapp.com/map");
+        Assert.assertEquals(driver.getTitle(),"Find Items In-Store"); // Each title matches the URl pattern (except the map, see navigatePages function)
+        driver.quit();
+    }
+
+    @Test
+    public void checkMapHeader() throws Exception{
+        WebDriver driver = new HtmlUnitDriver();
+        driver.get("https://phabpharmacy.herokuapp.com/map");
+        WebElement header = driver.findElement(By.name("mapResponse"));
+        Assert.assertEquals(header.getText(),"Cold and Flu");
+        driver.quit();
+    }
+
+    @Test
+    public void checkProductList() throws Exception{
+        WebDriver driver = new HtmlUnitDriver();
+        driver.get("https://phabpharmacy.herokuapp.com/map");
+
+        Actions a = new Actions(driver);
+        WebElement browse = driver.findElement(By.name("Browse"));
+        WebElement dropDown = driver.findElement(By.className("dropdown-content"));
+        List<WebElement> dropDownElements = dropDown.findElements(By.xpath(".//*"));
+
+        for (int i=0;i<dropDownElements.size();i++){
+            a.moveToElement(browse).build().perform();
+            a.moveToElement(dropDownElements.get(i)).click().perform();
+            System.out.println((i+1)+":"+driver.getCurrentUrl());
+        }
+
+    }
+    
 }
